2d8c168ff8035620fb37daf41c85a112360c67ff
Chet Ram <56634108+chetramewb@users.noreply.github.com>
2020-10-22 17:17:27 +0530

    style work in progress

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+
+  $('.slide-full').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    direction: 1,
+    nextArrow: '<button class="next-arrow"><i class="fas fa-arrow-right"></i> </button>', 
+    prevArrow: '<button class="back-arrow"> <i class="fas fa-arrow-left"></i> </button>', 
+    responsive: [
+    
+    {
+    breakpoint: 992,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+    }
+    },
+    {
+    breakpoint: 525,
+    settings: {
+    slidesToShow: 1,
+    slidesToScroll: 1
+    }
+    }
+    
+    ]
+    
+    });
+
+  
+
